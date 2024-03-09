2d54aaef80a3b35963e55fec4f8cc81343cb1611
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-09 07:00:36 -0800

    Update App.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,13 @@
 
+// import { Carousel } from 'react-responsive-carousel';
 import Header from './Componnents/Header/Header';
+import Caraousel from './Carasoul/Carousel';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <div>
+      <Header />
+      <Caraousel/>
     </div>
   );
 }
