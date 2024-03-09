f8b33488351045a4bca690138567beec82d9e1cf
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-09 07:00:29 -0800

    Create Data.js

---
+++ amazon-clone/src/Carasoul/Data.js
@@ -0,0 +1,8 @@
+import img1 from "./img/61CiqVTRBEL._SX3000_.jpg"
+import img2 from "./img/61lwJy4B8PL._SX3000_.jpg"
+import img3 from "./img/61zAjw4bqPL._SX3000_.jpg"
+import img4 from "./img/71Ie3JXGfVL._SX3000_.jpg"
+import img5 from "./img/71VBt4eTg3L._SX3000_.jpg"
+import img6 from "./img/81KkrQWEHIL._SX3000_.jpg"
+
+export const img= [img1,img2,img3,img4,img5,img6]
