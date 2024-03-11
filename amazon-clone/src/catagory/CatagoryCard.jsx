c5f42fe447943ce39cbb626b764fc03af6515b0d
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-11 00:58:01 -0700

    Create CatagoryCard.jsx

---
+++ amazon-clone/src/catagory/CatagoryCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react' 
+import classes from "./catagory.module.css"
+function CtagoryCard({data}) {
+  return (
+    <div className={classes.catagory}>
+          <a href=''>
+              <span>
+          <h2>{data.title}</h2>
+              </span>
+              <img src={data.imgLink} alt="" />
+              <p>Shop Now</p>
+      </a>
+    </div>
+  )
+}
+
+export default CtagoryCard
