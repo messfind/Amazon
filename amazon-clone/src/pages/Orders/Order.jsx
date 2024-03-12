4dbcb0b1076a84d853712d1cc3457c0726625463
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-12 01:53:49 -0700

    Create Order.jsx

---
+++ amazon-clone/src/pages/Orders/Order.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LayOut from '../../Componnents/LayOut/LayOut'
+function Order() {
+  return (
+
+    <LayOut>
+      <h1>Orders</h1>
+      </LayOut>
+   
+  )
+}
+
+export default Order
