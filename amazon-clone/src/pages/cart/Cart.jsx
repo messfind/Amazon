4fe132a6ba440e74145bce692585c1220da2bfcb
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-12 01:56:31 -0700

    Create Cart.jsx

---
+++ amazon-clone/src/pages/cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LayOut from '../../Componnents/LayOut/LayOut'
+
+function Cart() {
+    return ( 
+        <LayOut>
+            <h1>Cart</h1>
+    </LayOut>  
+  )
+}
+
+export default Cart
