684a3d01a746e25386963843b19d9bb8ad02b766
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-12 01:53:04 -0700

    Create Router.jsx
    Routing pages

---
+++ amazon-clone/src/pages/Router.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./Landing"
+import SignUp from "./Auth/SignUp"
+import Pyment from "./pyment/Pyment"
+import Order from "./Orders/Order"
+import Cart from "./cart/Cart"
+function Routing() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/auth" element={<SignUp />} />
+                <Route path="/payments" element={<Pyment />} />
+                <Route path="/orders" element={<Order />} />
+                <Route path="/cart" element={<Cart/>} />
+                    
+
+            </Routes>
+      </Router>
+  )
+}
+
+export default Routing
