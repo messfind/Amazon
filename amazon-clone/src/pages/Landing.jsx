c21db284ec21b2825b7e5a921f48b4d84412de09
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-12 07:04:58 -0700

    Update Landing.jsx

--- amazon-clone/src/pages/Landing.jsx
+++ amazon-clone/src/pages/Landing.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Carousel from "../Carasoul/Carousel"
 import Category from "../category/Category"
+import Product from './Product'
 import LayOut from '../Componnents/LayOut/LayOut'
 function Landing() {
   return (
      <LayOut>
           <Carousel />
-          <Category/>
+      <Category />
+      <Product/>
      </LayOut>
   )
 }
