a1f94206db4dba7ee3657acc0898c9d93f9eb301
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-12 06:29:21 -0700

    Create CategoryInfo.js

---
+++ amazon-clone/src/category/CategoryInfo.js
@@ -0,0 +1,26 @@
+export const CategoryInfos = [
+    {
+        id:1,
+        title: "Electronics",
+        name: "electronics",
+        imgLink:"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_TV_2X._SY304_CB432517900_.jpg"
+    },
+    {
+        id:2,
+        title: "Jewelry",
+        name: "jewelry",
+        imgLink:"https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
+    },
+    {
+        id:3,
+        title: "Women's clothing",
+        name: "Clothing",
+        imgLink:"https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg"
+    },
+    {
+        id:4,
+        title: "Men's clothing",
+        name: "men's cloth",
+        imgLink:"https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
+    }
+]
