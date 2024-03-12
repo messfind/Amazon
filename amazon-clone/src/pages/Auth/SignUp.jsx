49d301ca42b9c1edc92eae6ff82adde4c0c81b17
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-12 01:56:33 -0700

    Create SignUp.jsx

---
+++ amazon-clone/src/pages/Auth/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LayOut from '../../Componnents/LayOut/LayOut'
+
+function SignUp() {
+    return (
+        <LayOut>
+            <h1>Sign Up</h1>
+      </LayOut>
+  )
+}
+
+export default SignUp
