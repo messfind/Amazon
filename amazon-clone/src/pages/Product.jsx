b34dc44fde266290b52ad9d33d5a9a7accabc14d
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-12 23:25:53 -0700

    Update Product.jsx
    product list

--- amazon-clone/src/pages/Product.jsx
+++ amazon-clone/src/pages/Product.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+// const jewelry = "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg";
+import { Link } from "react-router-dom"
+import classes from "../Componnents/Header/Header.module.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 function Product() {
   return (
     <div>
-      
+      <div className={classes.product}>
+      <Link to=''>
+        <img src="https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg" alt="" />
+      </Link>
+      <Link to=''>
+  <img src="https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg" alt="" />
+        </Link>
+        <Link to=''>
+   <img src="https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg" alt="" />
+        </Link>
+        <Link to=''>
+  <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="" />
+     </Link>
+      </div>
+      {/* <div> */}
+      {/* <Link to=''> */}
+   {/* <img src="https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg" alt="" /> */}
+        {/* </Link> */}
+        {/* <Link to=''> */}
+  {/* <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="" /> */}
+     {/* </Link> */}
+      {/* </div> */}
     </div>
   )
 }
